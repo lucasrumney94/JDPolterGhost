086399ec23bb391d28565584fd4cd0b20e2b32c5
Lucas Rumney <lucasrumney94@gmail.com>
2016-01-30 18:38:06 -0500

    Camera Rotation;

--- JDPolterGhost/Assets/cameraController.cs
+++ JDPolterGhost/Assets/cameraController.cs
@@ -11,7 +11,7 @@ public class cameraController : MonoBehaviour
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="deltaElevation"></param>
+    /// <param name="deltaElevation">[-1,1] amount of change in elevation</param>
     public void changeElevation(float deltaElevation)
     {
 
