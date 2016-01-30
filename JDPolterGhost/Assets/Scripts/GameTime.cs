48c7416d9216bf50754c1ea43a9f2cbafec4a25a
Hardy Whitney <htw6174@gmail.com>
2016-01-29 19:14:38 -0500

    improved navmeshtest, added basic game time checking function

---
+++ JDPolterGhost/Assets/Scripts/GameTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameTime 
+{
+    public static float timeScaleFactor = 0.01f;
+
+	public static float TimeOfDay()
+    {
+        float time;
+
+        time = Time.time % 3600;
+
+        return time * timeScaleFactor;
+    }
+}
---
+++ JDPolterGhost/Assets/Scripts/TestScripts/AITest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AITest : MonoBehaviour
+{
+    public GameObject destination;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+	void Update()
+    {
+        agent.SetDestination(destination.transform.position);
+    }
+}
