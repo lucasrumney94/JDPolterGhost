ac58f289ee709217cb408775aa8a0ecae42d580c
Hardy Whitney <htw6174@gmail.com>
2016-02-02 01:01:52 -0500

    Updated AIAgentBase class; rebaked my test scene navmesh; various small fixes to other scripts

---
+++ JDPolterGhost/Assets/Scripts/Editor/ScheduleDrawer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomPropertyDrawer(typeof(ScheduleItem))]
+public class ScheduleDrawer : PropertyDrawer
+{
+    float timeWidth = 40;
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        int indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        float leftOverWidth = position.width - (2 * timeWidth) - 10;
+
+        Rect objectRect = new Rect(position.x, position.y, leftOverWidth, position.height);
+        Rect startTimeRect = new Rect(position.x + leftOverWidth + 5, position.y, 40, position.height);
+        Rect endTimeRect = new Rect(position.x + leftOverWidth + timeWidth + 10, position.y, 40, position.height);
+
+        EditorGUI.PropertyField(objectRect, property.FindPropertyRelative("location"), GUIContent.none);
+        EditorGUI.PropertyField(startTimeRect, property.FindPropertyRelative("startTime"), GUIContent.none);
+        EditorGUI.PropertyField(endTimeRect, property.FindPropertyRelative("endTime"), GUIContent.none);
+
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+}
---
+++ JDPolterGhost/Assets/Scripts/Structs/ScheduleItem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class ScheduleItem
+{
+	public GameObject location;
+
+    public float startTime;
+    public float endTime;
+}
