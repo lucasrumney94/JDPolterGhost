3c6a5bd6cd1cbb3899a89247859d0ddc38c26e6c
Hardy Whitney <htw6174@gmail.com>
2016-02-04 18:38:47 -0500

    Changed player to use velocity-based movement, turn to rotate camera only when moving; updated InputManager to support sensitivity settings and inverted look

--- JDPolterGhost/Assets/Scripts/Player/PlayerMovement.cs
+++ JDPolterGhost/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,13 +8,20 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
     public static GameObject player;
+    public GameObject playerCamera;
 
+    public bool forceMovement = false;
     public float speed = 5.0f;
+    [Range(0f, 1f)]
+    public float rotationSpeed = 0.1f;
+
+    private Rigidbody playerRigidbody;
 
     void Start()
     {
         player = this.gameObject;
-
+        playerCamera = GameObject.FindGameObjectWithTag(Tags.camera);
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
 
@@ -24,7 +31,27 @@ public class PlayerMovement : MonoBehaviour
     /// <param name="movementVector">Direction of movement</param>
     public void MovePlayer(Vector3 movementVector)
     {
-        player.GetComponent<Rigidbody>().AddRelativeForce(movementVector);
+        if (forceMovement) MoveForce(movementVector);
+        else MoveVelocity(movementVector);
+        if(movementVector.magnitude > 0.01f)
+        {
+            RotateToCamera();
+        }
+    }
+
+    private void MoveForce(Vector3 movementVector)
+    {
+        playerRigidbody.AddRelativeForce(movementVector);
+    }
+
+    private void MoveVelocity(Vector3 movementVector)
+    {
+        playerRigidbody.velocity = transform.TransformVector(movementVector) * speed;
+    }
+
+    private void RotateToCamera()
+    {
+        player.transform.rotation = Quaternion.Slerp(transform.rotation, playerCamera.transform.rotation, rotationSpeed);
     }
 
     /// <summary>
