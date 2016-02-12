2e9bf6016bbddceceb6d8c95e8d38191aa9f4d80
Hardy Whitney <htw6174@gmail.com>
2016-02-11 20:42:20 -0500

    Added GameController; interactions now reduce player influence

---
+++ JDPolterGhost/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public bool dadScaredOff = false;
+    public bool momScaredOff = false;
+
+    public AIAgentBase dadAgent;
+    public AIAgentBase momAgent;
+
+    void Update()
+    {
+        if(dadAgent.fearLevel > dadAgent.maxFearLevel)
+        {
+            dadScaredOff = true;
+        }
+        if (momAgent.fearLevel > momAgent.maxFearLevel)
+        {
+            momScaredOff = true;
+        }
+        if(dadScaredOff && momScaredOff)
+        {
+            //game won
+        }
+    }
+}
