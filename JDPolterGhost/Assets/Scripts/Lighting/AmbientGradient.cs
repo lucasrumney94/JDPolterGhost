aaee9b80ed9a964a19b502837c244176037ef069
Hardy Whitney <htw6174@gmail.com>
2016-02-24 20:18:39 -0500

    Ambient light gradient

---
+++ JDPolterGhost/Assets/Scripts/Lighting/AmbientGradient.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class AmbientGradient : MonoBehaviour
+{
+    public Gradient skyColor;
+
+    private Light ambientLight;
+
+    void Start()
+    {
+        ambientLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        float gradientPoint = GameTime.TimeOfDay() * (1f / 24f);
+        ambientLight.color = skyColor.Evaluate(gradientPoint);
+    }
+}
