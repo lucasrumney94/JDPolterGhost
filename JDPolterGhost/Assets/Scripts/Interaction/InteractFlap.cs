bf270e47f999c4dc6791a83bc88a15130581db3a
Hardy Whitney <htw6174@gmail.com>
2016-02-25 21:01:25 -0500

    Flap interaction script for opening/closing doors and such

---
+++ JDPolterGhost/Assets/Scripts/Interaction/InteractFlap.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractFlap : InteractableObject
+{
+    public GameObject arm; //Object to rotate, should be an empty object at the hinge point of what you're rotating
+    
+    public Vector3 rotationAxis; //Make negative to reverse direction, change magnitude to speed up rotation
+
+    public float angleLimit; //Max angle in degrees
+
+    public Vector3 originalRotation;
+
+    public bool flapping;
+    public bool returningToStart = false;
+
+    void Start()
+    {
+        originalRotation = arm.transform.eulerAngles;
+    }
+
+    void Update()
+    {
+        if (flapping)
+        {
+            Rotate();
+        }
+    }
+
+    public override bool Activate()
+    {
+        if (spookey == false)
+        {
+            spookey = true;
+            flapping = true;
+            return true;
+        }
+        return false;
+    }
+
+    private void Rotate()
+    {
+        if (returningToStart)
+        {
+            float previousAngleDiff = Vector3.Distance(originalRotation, arm.transform.eulerAngles);
+            arm.transform.eulerAngles -= Time.deltaTime * rotationAxis;
+            float currentAngleDiff = Vector3.Distance(originalRotation, arm.transform.eulerAngles);
+            if(currentAngleDiff > previousAngleDiff) //Has the rotation gone past the original position?
+            {
+                returningToStart = false;
+                flapping = false;
+                spookey = false;
+                ClearFrightenedAgents();
+                arm.transform.eulerAngles = originalRotation;
+            }
+        }
+        else
+        {
+            arm.transform.eulerAngles += Time.deltaTime * rotationAxis;
+            if(Vector3.Distance(originalRotation, arm.transform.eulerAngles) > angleLimit)
+            {
+                returningToStart = true;
+            }
+        }
+    }
+}
